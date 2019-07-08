6737145c2a45d545cb0aa919df96762d0912a7e0
Narutofox <Narutofox@users.noreply.github.com>
2019-07-08 12:02:47 +0200

    HTTP -> HTTPS

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://localhost:8080/api/"',
+  BASE_URL: '"https://localhost:44375/api/"'
 })
